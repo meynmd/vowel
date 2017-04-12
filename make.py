2955d31332cf1505dd6339dca252303602f63692
dimbul <durgaharish1993@gmail.com>
2017-04-12 09:25:46 -0700

    new make file

--- make.py
+++ make.py
@@ -1,58 +1,152 @@
-import sys
+from collections import defaultdict
 
-'''
-MakeWordRecognizer(name, word)
+def split_string(split_str,fsa_dict):
+    cur_state = 's0'
+    cons_str = None
 
-takes the name which will be prepended to all state names,
-and the word to recognize
+    for i in range(len(split_str)):
+        if split_str[i] in fsa_dict[cur_state]:
+            cur_state = fsa_dict[cur_state][split_str[i]]
+            continue
+        else:
+            cons_str = split_str[i:]
+            break
 
-returns (first state, accept state, rules)
-'''
-def MakeWordRecognizer(name, word):
-    rec = ''
-    count = 0
-    for c in word:
-        if c == ' ' or c == '\n':
-	    continue
-
-        count += 1
-
-        # write the string for this transition rule
-        rule = '(' + name + str(count) + \
-            ' (' + name + str(count + 1) + \
-            ' ' + c + '))\n'
-
-        rec = rec + rule
-
-    return (rec, name + '1', name + str(count + 1))
+    return cons_str,cur_state
 
 
 
-'''
-MakeLanguageRecognizer(words)
+def construct_fsa(cons_str,cur_state,fsa_dict,cur_final_state=0):
+    state_count=cur_final_state
+    for i in range(len(cons_str)):
 
-takes words, a list of words in the language to be recognized
+        if cur_state not in fsa_dict:
+            fsa_dict[cur_state] = defaultdict(str)
 
-returns a FSA for that list of words
-'''
-def MakeLanguageRecognizer(words):
-    fsa = 'F\n'
-    fsa = fsa + '(S (S _))\n'
-    count = 0
-    for word in words:
-        #print 'processing word ' + word + '...'
-        count += 1
-        wr = MakeWordRecognizer(str(count) + '_', word)
-        wordRec = wr[0]
-        initState = wr[1]
-        finalState = wr[2]
-        fsa = fsa + '(S (' + initState + ' *e*))\n'
-        fsa = fsa + wordRec
-        fsa = fsa + '(' + finalState + ' (S *e*))\n'
-    
-    fsa = fsa + '(S (F *e*))\n'
-    return fsa
+        if cons_str[i] == ' ':
+            fsa_dict[cur_state][cons_str[i]] = cur_state
+            continue
 
-lines = sys.stdin.readlines()
-language = MakeLanguageRecognizer(lines)
-print(language)
+        new_state = str(state_count + 1)
+        if cons_str[i] not in fsa_dict[cur_state]:
+            fsa_dict[cur_state][cons_str[i]] = new_state
+        cur_state = new_state
+        state_count+=1
+    if new_state  not in fsa_dict:
+        fsa_dict[new_state]=defaultdict(str)
+        fsa_dict[new_state]['eplison'] = 'f_state'
+    else:
+        fsa_dict[new_state]['eplison'] = 'f_state'
+    return fsa_dict,int(new_state)
+
+
+
+
+def check_fsa(cur_str,fsa_dict):
+    cur_state = 's0'
+    #accepted = True
+
+    for ch in cur_str:
+        if ch in fsa_dict[cur_state]:
+            cur_state=fsa_dict[cur_state][ch]
+            continue
+        else:
+            break
+            accepted=False
+
+    if 'eplison' in fsa_dict[cur_state]:
+        accepted = True
+    else:
+        accepted = False
+
+
+    return accepted
+
+
+
+
+def convert_fsa_carmel(fsa_dict):
+
+    #fsa_dict={'1': {'A': '2', ' ': '1'}, '3':  {'eplison': 'f_state'}, '2':  {'eplison': 'f_state', 'B': '3', ' ': '2'}, 's0': {'A': '1'},'f_state': {'_': 's0'}}
+
+
+
+    print 'f_state'
+
+    for key1 in fsa_dict['s0']:
+        if key1=='eplison':
+            key1_d = '*e*'
+        else:
+            key1_d = key1
+
+        print '(', 's0', '(', fsa_dict['s0'][key1], key1_d, '))'
+
+
+    for key in fsa_dict:
+        if key=='s0':
+            continue
+
+
+
+        for key1 in fsa_dict[key]:
+            # if key=='s0':
+            #     key_d = '0'
+            # else:
+            #     key_d = key
+
+            if key1=='eplison':
+                key1_d = '*e*'
+            else:
+                key1_d = key1
+
+
+            print '(',key,'(',fsa_dict[key][key1],key1_d,'))'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    data=[]
+    with open('vocab','rb') as fp:
+        for line in fp.readlines():
+            if '\n' in line:
+                data+=[line[:-1]]
+            else:
+                data+=[line]
+
+
+    fsa_dict = defaultdict(dict)
+    fsa_dict['s0']=defaultdict(str)
+
+    f_state=0
+    for item in data:
+        end_str,end_state=split_string(item,fsa_dict)
+        if end_str!=None:
+            fsa_dict,f_state=construct_fsa(end_str,end_state,fsa_dict,f_state)
+
+    fsa_dict['f_state']['_'] = 's0'
+    convert_fsa_carmel(fsa_dict)
+
+
+
+
+
+
+
+
+
+
