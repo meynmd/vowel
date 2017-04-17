4d45b380f91d1047cfae8e56f86c7c08d74d12a0
Matthew Meyn <meynm@flip1.engr.oregonstate.edu>
2017-04-16 21:41:06 -0700

    l2w.py

---
+++ l2w.py
@@ -0,0 +1,36 @@
+def makeTransitions(word, name):
+    output = ''
+    count = 0
+
+    output = '(F (' + name + '_' + '0 *e* *e*))\n'
+    currentState = name + '_' + '0'
+
+    for letter in word:
+        count += 1
+        if letter == ' ' or letter == '\n':
+            continue
+
+        if count < len(word):
+            nextState = name + '_' + str(count)
+            output = output + '(' + currentState + ' (' + nextState + ' ' + letter + ' *e*))\n'
+            currentState = nextState
+        else:
+            output = output + '(' + currentState + ' (F ' + letter + ' \"' + word + '\"))\n'
+
+    return output
+
+
+if __name__ == '__main__':
+    txt = ''
+    with open('vocab.small', 'r') as infile:
+        txt = infile.read()
+    
+    words = txt.split('\n')
+    fst = ''
+    count = 0
+    for word in words:
+        count += 1
+        word = word.strip()
+        fst = fst + makeTransitions(word, str(count)) + '\n'
+
+    print(fst)
